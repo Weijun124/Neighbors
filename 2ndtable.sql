66d895f687ef06ce2cd3503852db68bf745d29de
Weijun124 <98248068+Weijun124@users.noreply.github.com>
2024-05-19 15:16:46 -0400

    Update 2ndtable.sql

--- 2ndtable.sql
+++ 2ndtable.sql
@@ -2,14 +2,15 @@ DROP TABLE IF EXISTS Accounts CASCADE;
 DROP TABLE IF EXISTS Blocks CASCADE;
 DROP TABLE IF EXISTS Neighbors CASCADE;
 DROP TABLE IF EXISTS Neighbor CASCADE;
-DROP TABLE IF EXISTS Usermember CASCADE;
 DROP TABLE IF EXISTS Friendship CASCADE;
 DROP TABLE IF EXISTS Membership CASCADE;
 DROP TABLE IF EXISTS neighborhoods CASCADE;
 DROP TABLE IF EXISTS accountprofile CASCADE;
 DROP TABLE IF EXISTS threads CASCADE;
+DROP TABLE IF EXISTS blockfollow CASCADE;
 
-CREATE TABLE Neighbors (
+
+CREATE TABLE Neighborhoods (
     nid int,
     name VARCHAR(255),
 	PRIMARY KEY (nid)
@@ -26,7 +27,7 @@ CREATE TABLE Blocks (
     FOREIGN KEY (nid) REFERENCES Neighbors
 );
 Create Table Accounts(
-	accountname varchar,--build the check function make sure there is no same AccountName before sign up this accountName
+	accountname varchar,
 	passwords varchar,
 	email varchar,
 	firstname varchar,
@@ -35,6 +36,7 @@ Create Table Accounts(
 	latitude float,
     longitude float,
 	block_bid int,
+	introduction text,
 	PRIMARY KEY(accountname),
 	FOREIGN KEY (block_bid) REFERENCES Blocks(bid)
 );
@@ -44,16 +46,63 @@ Create Table Friendship(
 	username varchar,
 	friendname varchar,
 	Status char(10),
-	PRIMARY KEY(id)
+	PRIMARY KEY(id),
+	FOREIGN KEY (username) REFERENCES Accounts(accountname),
+	FOREIGN KEY (friendname) REFERENCES Accounts(accountname)
 );
 
---once user sign up, it automatic require approve from the block memeber for be the member
-Create Table Usermember(
-	
+Create Table Membership(
+	blockid int,
+	requesterid varchar,
+	memberstatus varchar,
+	requeststatus boolean,
+	count int,
+	FOREIGN KEY (requesterid) REFERENCES Accounts(accountname),
+	FOREIGN KEY (blockid) REFERENCES Blocks(bid)
+)
+
+Create Table Blockfollow(
 	accountname varchar,
-	membershipstatus varchar,
 	bid int,
 	FOREIGN KEY (accountname) REFERENCES Accounts,
 	FOREIGN KEY (bid) REFERENCES Blocks
 )
+
+Create Table Neighbors(
+	userid varchar,
+	neighborid varchar,
+	FOREIGN KEY (userid) REFERENCES Accounts(accountname),
+	FOREIGN KEY (neighborid) REFERENCES Accounts(accountname)
+)
+
+Create Table Threads(
+	threadid int,
+	blockid int,
+	body text,
+	feedType char[10],
+	receiver varchar,
+	subject varchar,
+	time timestamp,
+	accountname varchar,
+	Primary Key (threadid),
+	FOREIGN KEY (accountname) REFERENCES Accounts,
+	FOREIGN KEY (receiver) REFERENCES Accounts(accountname)
+)
+
+Create Table Messages(
+	InitialThreadID int,
+	ReplyID int,
+	Reply text,
+	postTime timestamp,
+	FOREIGN KEY(InitialThreadID) REFERENCES Threads(threadid),
+	FOREIGN KEY(ReplyID) REFERENCES Accounts(Accountname)
+);
+
+Create Table UserReads(
+	InitialThreadID int,
+	AccountID int,
+	LastRead timestamp,
+	FOREIGN KEY(InitialThreadID) REFERENCES Threads(threadid),
+	FOREIGN KEY(AccountID) REFERENCES Accounts(accountname)	
+);
 
